d527be629e0f5c729fa909418487d8fc22a4b68c
James Rezendes <jamierezendes@gmail.com>
2022-01-16 12:48:37 -0800

    Debug

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 let cameraRig;
 let page;
+let isMobile = false;
+if( window.DeviceOrientationEvent && navigator.userAgent.match(/iPhone/i)
+  || navigator.userAgent.match(/webOS/i)
+  || navigator.userAgent.match(/Android/i)
+  || navigator.userAgent.match(/iPad/i)
+  || navigator.userAgent.match(/iPod/i)
+  || navigator.userAgent.match(/BlackBerry/i)
+  || navigator.userAgent.match(/Windows Phone/i)
+){
+  isMobile = true;
+}
 
 window.onload = async () => {
   page = document.querySelector(".page");
@@ -10,13 +21,18 @@ window.onload = async () => {
 valoria.onJoin = async () => {
   console.log("SIGNED IN!");
   page.style.display = "none";
-  controls.lock();
+  if(!isMobile){
+    controls.lock();
+  }
+  alert(1)
   valoria.dimension.onPeerJoin = (id) => {
     addPeerToScene(id);
   }
+  alert(2)
   valoria.dimension.onPeerLeave = (id) => {
     removePeerFromScene(id);
   }
+  alert(3)
   await valoria.joinDimension("Valoria");
 }
 
