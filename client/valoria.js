f535d6672c03de4b03180f87b79bf8cbef9e69e9
James Rezendes <jamierezendes@gmail.com>
2022-04-09 11:17:36 -0700

    Mobile webrtc debug

--- client/valoria.js
+++ client/valoria.js
@@ -4299,7 +4299,7 @@ class Valoria {
     const url = data.url;
     const polite = data.polite;
     if(self.peers[url] && self.peers[url]?.datachannel?.open && self.peers[url]?.readyState == "open") return;
-    if(self.peers[url] && description.type == "offer" && self.peers[url].signalingState !== "stable") delete self.peers[url];
+    if(self.peers[url] && description.type == "offer") delete self.peers[url];
     if(!self.peers[url]){
       self.peers[url] = new RTCPeerConnection({iceServers});
       self.peers[url].onStream = self.peers[url].onStream || (() => {});
